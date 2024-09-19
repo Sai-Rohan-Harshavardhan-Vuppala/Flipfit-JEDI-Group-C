44db3068f57b567ac7a9917791e8826a233b6bf3
Pratishtha2616 <141442145+Pratishtha2616@users.noreply.github.com>
2024-09-19 15:46:09 +0530

    Admin client

--- JEDI-Group-C-Flipkart-JAVA-POS/src/flipfit/flipkart/client/FlipFitAdminClient.java
+++ JEDI-Group-C-Flipkart-JAVA-POS/src/flipfit/flipkart/client/FlipFitAdminClient.java
@@ -1,16 +1,51 @@
 package flipfit.flipkart.client;
 
+import flipfit.flipkart.bean.FlipFitGym;
+import flipfit.flipkart.bean.FlipFitSlot;
+import flipfit.flipkart.bean.FlipFitUser;
+
+
 import java.util.Scanner;
 
+import static java.lang.System.in;
+
 public class FlipFitAdminClient {
+
+    public static void approveUser() {
+        System.out.println("Approved User details");
+    }
+    public static void validateGym(){
+        System.out.println("Gym validated");
+
+    }
+    public static void validateSlot(){
+        System.out.println("Slot validated");
+
+    }
     public boolean showMenu(){
         System.out.println("Welcome to FlipFit Admin Client");
-        System.out.println("1. Logout");
-        Scanner scanner = new Scanner(System.in);
-        int choice = Integer.parseInt(scanner.nextLine());
+        System.out.println("1. Approve user");
+        System.out.println("2. Validate Gym");
+        System.out.println("3. Validate Slot");
+        System.out.println("4. Logout");
+
+        Scanner sc = new Scanner(in);
+
+
+        int choice = Integer.parseInt(sc.nextLine());
+
         switch(choice){
-            case 1:
-                return true;
+                    case 1:{
+                        validateGym();
+                        break;
+                    }
+                    case 2:{
+                        validateSlot();
+                        break;
+                    }
+                    default: {
+                        System.out.println("Invalid choice");
+                    }
         }
         return false;
     }
