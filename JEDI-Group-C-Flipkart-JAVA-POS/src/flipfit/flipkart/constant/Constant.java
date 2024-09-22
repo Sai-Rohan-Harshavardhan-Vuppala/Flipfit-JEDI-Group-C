fc1b88812f9fc6e99f6b1e5d1617211a353fdc00
Sai Rohan Harshavardhan Vuppala <shv12@iitbbs.ac.in>
2024-09-22 13:57:21 +0530

    tables.sql added

--- JEDI-Group-C-Flipkart-JAVA-POS/src/flipfit/flipkart/constant/Constant.java
+++ JEDI-Group-C-Flipkart-JAVA-POS/src/flipfit/flipkart/constant/Constant.java
@@ -2,6 +2,6 @@ package flipfit.flipkart.constant;
 
 public class Constant {
     public static final String DATABASE_USER = "root";
-    public static final String DATABASE_PASSWORD = "yiagh2Sh!@#%";
+    public static final String DATABASE_PASSWORD = "";
     public static final String DATABASE_URL = "jdbc:mysql://localhost:3306/FlipFitDatabase";
 }
